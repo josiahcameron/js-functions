9cc6ecab39ab684c25f40ae15b8dcb7b8aaea8fd
josiahcameron <josiahcameron2015@gmail.com>
2023-01-04 09:06:19 -0500

    Working on exercises.js

--- js/exercises.js
+++ js/exercises.js
@@ -4,30 +4,61 @@
   // ---------------------
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
-
+  function max(num1, num2){ //Defines max as using the parameters num1 and num2
+    if (num1 > num2){
+      return num1 
+    } else {                //if num one is greater, then it's value will be returned. Since num2 is the only other option, it's what will be returned.
+      return num2
+    }
+  }
 
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
-
-
+  function maxOfThree(num1, num2, num3){
+    const maxNum = Math.max(num1, num2, num3); //Math.max() takes the largest number put in the parameters. In this case it takes the largest from num1, num2, and num3.
+    return maxNum; //maxNum calls the result of Math.max() and returns the value.
+  }
 
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
-
-
+  function isVowel(str){
+    if (str === 'a' || str === 'e' || str === 'i' || str ===  'o' || str === 'u'){
+      return true;
+    } else {
+      return false;
+    }
+  }
+    //"||" (aka "or") checks each argument individually within the if statement. "===" ensures that it only check if it's the exact character.
 
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
-
-
+  function rovarspraket(str){
+    const characters = str.split(""); //this will create an array of all characters broken apart in whatever the input is for rocarspraket
+    const len = characters.length;
+    return len
+  }
+  //Test the text for all variables that aren't a, e, i, o, or u and double them. No idea how I'm going to do this.
 
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
+  function sum(array){
+    let sum = 0
+    for (let i = 0; i < array.length; i += 1) {
+      sum += array[i]
+    }
+    return sum
+  }
+//the initial sum is 0 then the length is taken from the array and added sequentially until there are no more items to add
+
+  function multiply(array){
+    let multiple = multiply.reduce((a,b) => a*b)
+    return multiple
+  }
 
 
 
