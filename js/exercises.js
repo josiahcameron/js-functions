444a65488499efe4d5f50d84c4fe180beab81c3e
josiahcameron <josiahcameron2015@gmail.com>
2023-01-23 09:16:59 -0500

    Updated Answers and Comments

--- js/exercises.js
+++ js/exercises.js
@@ -4,26 +4,31 @@
   // ---------------------
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
-  function max(num1, num2){ //Defines max as using the parameters num1 and num2
+  //Defines max as using the parameters num1 and num2
+  function max(num1, num2){ 
     if (num1 > num2){
       return num1 
-    } else {                //if num one is greater, then it's value will be returned. Since num2 is the only other option, it's what will be returned.
+  //if num one is greater, then it's value will be returned. Since num2 is the only other option, it's what will be returned.
+    } else {
       return num2
     }
   }
-
-
+  
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
   function maxOfThree(num1, num2, num3){
-    const maxNum = Math.max(num1, num2, num3); //Math.max() takes the largest number put in the parameters. In this case it takes the largest from num1, num2, and num3.
-    return maxNum; //maxNum calls the result of Math.max() and returns the value.
+    //Math.max() takes the largest number put in the parameters. In this case it takes the largest from num1, num2, and num3.
+    const maxNum = Math.max(num1, num2, num3); 
+    //maxNum calls the result of Math.max() and returns the value.
+    return maxNum; 
   }
 
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
+  
+  //"||" (aka "or") checks each argument individually within the if statement. "===" ensures that it only check if it's the exact character.
   function isVowel(str){
     if (str === 'a' || str === 'e' || str === 'i' || str ===  'o' || str === 'u'){
       return true;
@@ -31,29 +36,44 @@
       return false;
     }
   }
-    //"||" (aka "or") checks each argument individually within the if statement. "===" ensures that it only check if it's the exact character.
+  
+  //Correct:
+  /*function isVowel(char) {
+    return 'aeiou'.includes(char); -- Will automatically return a boolean
+  } 
+    */
+  // }
 
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
-  function rovarspraket(str){
-    const characters = str.split(""); //this will create an array of all characters broken apart in whatever the input is for rocarspraket
-    const len = characters.length;
-    return len
+
+  function rovarspraket(text) {
+    //.split() will return an array of characters
+    //The function will loop through each index of the text array. If the index value isn't a consonant, that index value will be a concatenation of the value, 'o', and the value again
+    text = text.split(''); 
+    for (let i = 0; i < text.length; i++) {
+    if('bcdfghjklmnpqrstvwxyz'.includes(text[i])) {
+    text[i] = text[i] + 'o' + text[i];
+   }
   }
-  //Test the text for all variables that aren't a, e, i, o, or u and double them. No idea how I'm going to do this.
+  //.join() will bring the array values together as a string
+  return text.join('');
+}
 
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
-  function sum(array){
-    let sum = 0
-    for (let i = 0; i < array.length; i += 1) {
-      sum += array[i]
-    }
-    return sum
+  //The initial sum is 0 then the length is taken from the array and added sequentially until there are no more items to add
+  
+  //arr.reduce loops through the array and executes function of the accumulated (acc) value and the current index; return the accumulated value + i
+  function sum(arr) {
+    return arr.reduce(function(acc, i) {
+      return acc + 1;
+    })
   }
-//the initial sum is 0 then the length is taken from the array and added sequentially until there are no more items to add
+  
+
 
   function multiply(array){
     let multiple = multiply.reduce((a,b) => a*b)
@@ -65,25 +85,52 @@
   // ---------------------
   // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
   // ---------------------
-
-
+  
+  // split() turns the string into an array, reverse() reverses the array values, join() brings the array together as a string.
+  function reverse(str) {
+    return str.split('').reverse().join('');
+  }
 
   // ---------------------
   // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
   // ---------------------
 
-
+  //Searches through array and calculates the length of the indeces. Will update the length variable with the largest value as it goes through the array.
+  function findLongestWord(arr) {
+    let length = 0;
+    for( let i = 0; i < arr.length; i++) {
+      if(arr[i].length > length) {
+        length = arr[i].length;
+      }
+    }
+  return length;
+  }
+  
 
   // ---------------------
   // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
   // ---------------------
+
+// filter() returns a new arrray that will only return array values with lengths that are shorter than the vale given to i.
+function filterLongWords(arr, i) {
+	arr.filter(function(word) {
+		word.length > i;
+	});
+}
 
 
 
   // ---------------------
   // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
   // ---------------------
-
+  function charFreq(str){
+    const freqList = [];
+    str.split('').forEach(function < char) {
+      freqList[char] = freqList[char] + 1 || 1;
+    };
+    return freqList;
+  }
+  
 
   ////////////////////////////////////////////////////////////////////////
   /////////////////////////DO NOT CHANGE CODE BELOW///////////////////////
